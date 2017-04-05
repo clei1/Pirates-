4aaeb8538158e0a40eb8e840f70a9c6552ce8e01
doniatung <doniatung32@gmail.com>
2017-04-05 12:57:33 -0400

    comments

--- DLLDeque.java
+++ DLLDeque.java
@@ -2,56 +2,59 @@ public class DLLDeque<T> implements Deque<T>{
     
     //add removeLast(), if the thing exists and where it exists, other stuffs	
 	
-    public DLLNode<T> first, last;
-    public int size;
-    
+    public DLLNode<T> first, last; //Nodes for the first and last element
+    public int size; // keep track of size
+
+
+    //Constructor 
     public DLLDeque() {
-	first = new DLLNode<T>(null, null, null);
-	last = new DLLNode<T>(null, null, null);
+	first = null;
+	last = null;
 	size = 0;
-    }   
+    }
     
+    //Method to add to the front of the Deque
     public void addFirst(T node){
-	first.setPrev(new DLLNode(node, null, first));
-	first = first.getPrev();  
+	first = new DLLNode(node, first);    
 	size++;
-    }
-	
-    public void addLast (T node) {
-	last.setNext(new DLLNode(node, last, null));
-	last = last.getNext();
-	size++;
-    }
-    
-    public T removeFirst() {
-	size--;
-	first = first.getNext();	
-	return first.setPrev(null).getCargo();
     }
 
-    public T removeLast(){
-	size--;
-	last = last.getPrev();
-	return last.setNext(null).getCargo();
+    //Method to add the the end of the Deque
+    public void addLast (T node) {
+	DLLNode nnode = new DLLNode(node, null);
+	last.setNext(nnode);
+	last = nnode;
+	size++;
     }
-	
+
+    //Removes the first element of the Deque
+    public void removeFirst() {
+	first = first.getNext();
+	size--;
+    }
+
+    //Returns the size of the Deque
     public int size() {
 	return size;
     }
-    
+
+    //Returns (does not remove) the first element of the Deque
     public T peekFirst() {
 	return first.getCargo();
     }
-    
+
+    //Returns (does not remove) the last element of the Deque
     public T peekLast() {
 	return last.getCargo();
     }
-    
+
+    //Returns a boolean, checks if the Deque is empty
     public boolean isEmpty() {
 	return size == 0;
     }
+
     
-    public static void main(String[] args) {
+    /* public static void main(String[] args) {
 	DLLDeque fred = new DLLDeque();
 	System.out.println(fred.isEmpty());
 	System.out.println(fred.size());
@@ -65,7 +68,7 @@ public class DLLDeque<T> implements Deque<T>{
 	fred.removeFirst();
 	System.out.println(fred.isEmpty());
 	System.out.println(fred.size());
-    }
+    }*/
 }
 
     
